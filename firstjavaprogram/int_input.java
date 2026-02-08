2b83ba4a3796de98055bb4852beb21b184654e38
KRISHNENDU MALICK <krishnendumalick07@gmail.com>
2026-02-09 02:00:56 +0530

    taking input string

--- firstjavaprogram/int_input.java
+++ firstjavaprogram/int_input.java
@@ -6,5 +6,9 @@ public class int_input {
     Scanner input = new Scanner(System.in);
     int rollno = input.nextInt();
     System.out.println("Your roll number is "+ rollno);
+
+    Scanner name = new Scanner(System.in);
+    String myname = name.next();
+    System.out.println(myname);
   }
 }
