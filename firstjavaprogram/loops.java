1d611557dcce8fbecb8497644977f6b3bec7ffa2
KRISHNENDU MALICK <krishnendumalick07@gmail.com>
2026-02-09 02:24:37 +0530

    Loops Basics

---
+++ firstjavaprogram/loops.java
@@ -0,0 +1,11 @@
+package firstjavaprogram;
+
+public class loops {
+  public static void main(String[] args) {
+    int count = 1;
+    while (count != 5){
+      System.out.println(count);
+      count++;
+    }
+  }
+}
