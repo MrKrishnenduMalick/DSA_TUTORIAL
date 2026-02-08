3b01477e12f69d2ceb7c045b105c2c813d3614a1
KRISHNENDU MALICK <krishnendumalick07@gmail.com>
2026-02-09 01:56:46 +0530

    Taking input of Integer value

---
+++ firstjavaprogram/int_input.java
@@ -0,0 +1,10 @@
+package firstjavaprogram;
+import java.util.Scanner;
+
+public class int_input {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int rollno = input.nextInt();
+    System.out.println("Your roll number is "+ rollno);
+  }
+}
